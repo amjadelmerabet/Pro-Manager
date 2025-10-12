e8c565ef4bc85b1fd2a3c2c8c6228446d0381c44
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2025-10-12 02:05:48 +0100

    Dashboard not showing user content issue fixed

--- server/controllers/tokens/checkAccessToken.js
+++ server/controllers/tokens/checkAccessToken.js
@@ -3,6 +3,8 @@ import pool from "../../db/connection.js";
 export default async function checkAccessToken(token) {
   try {
     const accessToken = await pool.query(
+      // For testing purpose only
+      // "SELECT token FROM tokens WHERE token = $1 AND expires > NOW() + INTERVAL '15 seconds'",
       "SELECT token FROM tokens WHERE token = $1 AND expires > NOW() + INTERVAL '1 minute'",
       [token]
     );
