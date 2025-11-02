48d865cb108c9c1e43cc2dd2aa155f48629673ce
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2025-11-03 00:16:30 +0100

    Fixed a bug that was causing the refresh token to be updated with a 15 minutes expiration time

--- server/controllers/tokens/updateToken.js
+++ server/controllers/tokens/updateToken.js
@@ -1,15 +1,19 @@
 import pool from "../../db/connection.js";
 
-export default async function updateToken(tokenId, token, username) {
+export default async function updateToken(tokenId, token, username, type) {
   try {
     const now = new Date();
     let expiresIn = new Date();
-    expiresIn.setMinutes(now.getMinutes() + 15);
+    if (type === "access") {
+      expiresIn.setMinutes(now.getMinutes() + 15);
+    } else if (type === "refresh") {
+      expiresIn.setHours(now.getHours() + 24 * 7);
+    }
     // For testing purposes only
     // expiresIn.setSeconds(now.getSeconds() + 120);
     const updatedAccessToken = await pool.query(
       "UPDATE tokens SET token = $1, expires = $2, updated_on = $3, updated_by = $4 WHERE token_id = $5",
-      [token, expiresIn, now, username, tokenId],
+      [token, expiresIn, now, username, tokenId]
     );
     return updatedAccessToken;
   } catch (error) {
