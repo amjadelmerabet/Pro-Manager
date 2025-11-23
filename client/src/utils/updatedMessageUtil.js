74c2205a1ec1829b7a83f35f4e68e3ef1d828c10
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2025-11-23 04:04:17 +0100

    Fixed some issues in the updated message util

--- client/src/utils/updatedMessageUtil.js
+++ client/src/utils/updatedMessageUtil.js
@@ -2,39 +2,89 @@ export default function updatedMessageUtil(update) {
   let now = new Date();
   const updatedYear = update.getUTCFullYear();
   const currentYear = now.getUTCFullYear();
-  if (updatedYear < currentYear) {
-    let difference = currentYear - updatedYear;
-    return `${difference} year${difference > 1 ? "s" : ""} ago`;
-  } else if (updatedYear === currentYear) {
-    const updatedMonth = update.getUTCMonth();
-    const currentMonth = now.getUTCMonth();
-    if (updatedMonth < currentMonth) {
-      let difference = currentMonth - updatedMonth;
-      return `${difference} month${difference > 1 ? "s" : ""} ago`;
-    } else if (updatedMonth === currentMonth) {
-      const updatedDate = update.getUTCDate();
-      const currentDate = now.getUTCDate();
-      if (updatedDate < currentDate) {
-        let difference = currentDate - updatedDate;
-        return `${difference} day${difference > 1 ? "s" : ""} ago`;
-      } else if (updatedDate === currentDate) {
-        const updatedHour = update.getUTCHours();
-        const currentHour = now.getUTCHours();
-        if (updatedHour < currentHour) {
-          let difference = currentHour - updatedHour;
-          return `${difference} hour${difference > 1 ? "s" : ""} ago`;
-        } else if (updatedHour === currentHour) {
-          const updatedMinute = update.getUTCMinutes();
-          const currentMinute = now.getUTCMinutes();
-          if (updatedMinute < currentMinute) {
-            let difference = currentMinute - updatedMinute;
-            return `${difference} minute${difference > 1 ? "s" : ""} ago`;
-          } else if (updatedMinute === currentMinute) {
-            const updatedSecond = update.getUTCSeconds();
+  const updatedMonth = update.getUTCMonth();
+  const currentMonth = now.getUTCMonth();
+  const updatedDate = update.getUTCDate();
+  const currentDate = now.getUTCDate();
+  const updatedHour = update.getUTCHours();
+  const currentHour = now.getUTCHours();
+  const updatedMinute = update.getUTCMinutes();
+  const currentMinute = now.getUTCMinutes();
+  const updatedSecond = update.getUTCSeconds();
             const currentSecond = now.getUTCSeconds();
-            if (updatedSecond < currentSecond) {
-              let difference = currentSecond - updatedSecond;
-              return `${difference} second${difference > 1 ? "s" : ""} ago`;
+  let yearDifference = currentYear - updatedYear;
+  if (yearDifference > 0) {
+    if (yearDifference === 1) {
+      let monthDifference = currentMonth - updatedMonth;
+      if (monthDifference >= 0) {
+        return "1 year ago";
+      } else {
+        return `${12 + monthDifference} month${12 + monthDifference > 1 ? "s" : ""} ago`;
+      }
+    } else {
+      return `${yearDifference} years ago`;
+    }
+  } else {
+    let monthDifference = currentMonth - updatedMonth;
+    if (monthDifference > 0) {
+      if (monthDifference === 1) {
+        let dateDifference = currentDate - updatedDate;
+        if (dateDifference >= 0) {
+          return "1 month ago";
+        } else {
+          let daysOfMonth = new Date(
+            updatedYear,
+            updatedMonth + 1,
+            0
+          ).getDate();
+          return `${daysOfMonth + dateDifference} day${daysOfMonth + dateDifference > 1 ? "s" : ""} ago`;
+        }
+      } else {
+        return `${monthDifference} month${monthDifference > 1 ? "s" : ""} ago`;
+      }
+    } else {
+      let dateDifference = currentDate - updatedDate;
+      if (dateDifference > 0) {
+        if (dateDifference === 1) {
+          let hourDifference = currentHour - updatedHour;
+          if (hourDifference >= 0) {
+            return "1 day ago";
+          } else {
+            return `${24 + hourDifference} hour${24 + hourDifference > 1 ? "s" : ""} ago`;
+          }
+        } else {
+          return `${dateDifference} days ago`;
+        }
+      } else {
+        let hourDifference = currentHour - updatedHour;
+        if (hourDifference > 0) {
+          if (hourDifference === 1) {
+            let minuteDifference = currentMinute - updatedMinute;
+            if (minuteDifference >= 0) {
+              return "1 hour ago";
+            } else {
+              return `${60 + minuteDifference} minute${60 + minuteDifference > 1 ? "s" : ""} ago`;
+            }
+          } else {
+            return `${hourDifference} hours ago`;
+          }
+        } else {
+          let minuteDifference = currentMinute - updatedMinute;
+          if (minuteDifference > 0) {
+            if (minuteDifference === 1) {
+              let secondDifference = currentSecond - updatedSecond;
+              if (secondDifference >= 0) {
+                return "1 minute ago";
+              } else {
+                return `${60 + secondDifference} second${60 + secondDifference > 1 ? "s" : ""} ago`;
+              }
+            } else {
+              return `${minuteDifference} minutes ago`;
+            }
+          } else {
+            let secondDifference = currentSecond - updatedSecond;
+            if (secondDifference > 0) {
+              return `${secondDifference} second${secondDifference > 1 ? "s" : ""} ago`;
             } else {
               return "now";
             }
