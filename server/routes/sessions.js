6c66bb7a7d48519fffab465d8a8d51243ff59217
Amjad EL MERABET <45132339+amjadelmerabet@users.noreply.github.com>
2026-05-24 17:49:41 +0100

    Deleted the API endpoint for creating a user session (Close #41)

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,57 +1,9 @@
-import createSession from "../controllers/sessions/createSession.js";
 import deleteSession from "../controllers/sessions/deleteSession.js";
-import linkSessionWithToken from "../controllers/tokens/linkSessionWithToken.js";
 
 export async function sessionsRoute(req, res) {
   const { method, url } = req;
   if (req.user) {
-    if (method === "POST") {
-      if (url === "/api/sessions/new") {
-        let body = "";
-        req.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-        req.on("end", async () => {
-          const { session, user, tokenId } = JSON.parse(body);
-          const newSession = await createSession(session, user);
-          if (newSession.error) {
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({
-                message: "Something went wrong while creating a new session",
-              }),
-            );
-          } else {
-            const sessionId = newSession?.rows[0].session_id;
-            const linkedToken = await linkSessionWithToken(sessionId, tokenId);
-            if (linkedToken.error) {
-              res.writeHead(500, { "Content-Type": "application/json" });
-              res.end(
-                JSON.stringify({
-                  message:
-                    "Something went wrong while linking the user session with the access token",
-                }),
-              );
-            } else {
-              res.writeHead(201, { "Content-Type": "application/json" });
-              res.end(
-                JSON.stringify({
-                  message: "New session created",
-                  session_id: sessionId,
-                }),
-              );
-            }
-          }
-        });
-      } else {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            message: "The endpoint that you are trying to reach doesn't exist",
-          }),
-        );
-      }
-    } else if (method === "DELETE") {
+    if (method === "DELETE") {
       if (url === "/api/sessions/delete") {
         let body = "";
         req.on("data", (chunk) => {
